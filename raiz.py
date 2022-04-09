957952018e71448f2286d4038c6c02ad2a3f9dc2
Alejandro Toledo Cuenca <alejandrotoledocuenca@gmail.com>
2022-04-09 13:47:03 -0500

    Algorithm

--- raiz.py
+++ raiz.py
@@ -1,16 +1,6 @@
 def run():
-    objetivo = int(input("Escribe un número: "))
-    respuesta = 0
-    
-    #fuerza bruta, prueba todo
-    while respuesta**2 < objetivo:
-        respuesta += 1
-
-    if respuesta**2 == objetivo:
-        print(f'La raíz cuadrada de {objetivo} es {respuesta}')
-    else:
-        print(f"{objetivo} no tiene raíz cuadrada exacta.")
-
+    numero = input("Escribe un número: ")
+    if numero 
 
 if __name__ == '__main__':
     run()
