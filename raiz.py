bf57948de19b1425ba880128691dcb403975015a
Alejandro Toledo Cuenca <alejandrotoledocuenca@gmail.com>
2022-04-09 14:14:39 -0500

    Only integers

--- raiz.py
+++ raiz.py
@@ -1,6 +1,11 @@
 def run():
-    numero = input("Escribe un número: ")
-    if numero 
+    numero = int(input("Escribe un número: "))
+    answer = numero**.5
+    if answer == int(answer):
+        answer = int(answer)
+        print(f"La raíz cuadrada de {numero} es {answer}")
+    else:
+        print(f"{numero} no tiene raíz cuadrada exacta.")
 
 if __name__ == '__main__':
     run()
